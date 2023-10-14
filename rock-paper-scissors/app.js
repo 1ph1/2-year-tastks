fb5a8f5b9746c3b978a6bc6d74765240bcb7665a
ddsdds <balovnev.artem@yandex.ru>
2023-10-14 20:13:23 +0700

    14.10.2023

--- rock-paper-scissors/app.js
+++ rock-paper-scissors/app.js
@@ -6,8 +6,13 @@ const pointsCompHTML = document.querySelector('.computer')
 const round = document.querySelector('.round')
 
 const settings = document.querySelector('.settings')
+const overlay = document.querySelector('overlay')
 const buttonSettingsOpen = document.querySelector('.button_settings_open')
 const buttonSettingsClose = document.querySelector('.button_settings_close')
+const roundNumbersPlayerChoise = document.querySelector('.round_player_choise')
+
+const restartButton = document.querySelector('.restart')
+console.log(restartButton)
 
 let pointsPlayer = 0
 let pointsComputer = 0
@@ -15,10 +20,12 @@ let roundNumber = 0
 
 buttonSettingsOpen.onclick = function (event) {
     settings.style.display = 'block';
+    overlay.style.display = 'block';
 }
 
 buttonSettingsClose.onclick = function (event) {
     settings.style.display = 'none';
+    overlay.style.display = 'none';
 }
 
 button.forEach((element) => element.addEventListener("click", function() {
@@ -33,10 +40,19 @@ function compChoise(array){
     return compResult
 }
 
+function reset(){
+    pointsPlayer = 0
+    pointsComputer = 0
+    roundNumber = 0
+    round.innerHTML = `start`
+    pointsPlayerHTML.innerHTML = "0"
+    pointsCompHTML.innerHTML = "0"
+}
+
+restartButton.addEventListener("click", reset)
+
 function gamePlay(playerChoise){
     let comp = compChoise(choise)
-    roundNumber++
-    round.innerHTML = `Round ${roundNumber}`
     if(playerChoise == comp){
         alert(`НИЧЬЯ! Компьютер выбрал ${comp}, а пользователь выбрал ${playerChoise}`)
     }else if(playerChoise != comp){
@@ -56,6 +72,20 @@ function gamePlay(playerChoise){
             pointsComputer++
             pointsCompHTML.innerHTML = pointsComputer
             alert(`ПОБЕДИЛ КОМПЬЮТЕР! Компьютер выбрал ${comp}, а пользователь выбрал ${playerChoise}`)
+        }
+    }
+    roundNumber++
+    round.innerHTML = `Round ${roundNumber}`
+    if(roundNumber==roundNumbersPlayerChoise.value){
+        if(pointsPlayer>pointsComputer){
+            alert("ПОБЕДА ИГРОКА!")
+            reset()
+        }else if(pointsComputer>pointsPlayer){
+            alert("ПОБЕДА КОМПЬЮТЕРА!")
+            reset()
+        }else{
+            alert("НИЧЬЯ!")
+            reset()
         }
     }
 }
